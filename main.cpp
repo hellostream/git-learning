c6b721e4edb348c9c054e360361382f6eeeec949
hellosweat <403576367@qq.com>
2020-09-11 14:49:08 +0800

    init main function

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-11 14:48:09
+ * @LastEditTime: 2020-09-11 14:48:33
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /git-learning/main.cpp
+ */
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
